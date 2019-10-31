43e2b8082b07e0c6fd97a2d6faa440ffbe67c2aa
kranolabar <particulargeek@gmail.com>
2019-10-31 17:07:58 +0800

    Changed login logic: - session to check for whether login is admin or user

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template, redirect, url_for, request
+
+from flask import Flask, render_template, redirect, url_for, request, session, abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('login_selection.html')
+    if not session.get('admin_logged_in') or session.get('user_logged_in'):
+        return render_template('login_selection.html')
+    elif session.get('admin_logged_in'):
+        return render_template('admin_home.html')
+    else:
+        return render_template('user_home.html')
 
 @app.route('/admin_login', methods=['POST'])
 def admin_login():
     if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['admin_logged_in'] = True
         return redirect(url_for('admin_home'))
     else
         flash('Invalid Entry')
@@ -17,6 +24,7 @@ def admin_login():
 @app.route('/user_login', methods=['POST'])
 def user_login():
     if request.form['password'] == 'password' and request.form['username'] == 'user':
+        session['user_logged_in'] = True
         return redirect(url_for('user_home'))
     else
         flash('Invalid Entry')
